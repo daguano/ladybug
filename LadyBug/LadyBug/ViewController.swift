3cc30de079cf31378b949847c43dabd5fa38a48a
daguano <daguano@gmail.com>
2016-11-05 15:48:59 -0200

    Projeto inicial

---
+++ LadyBug/LadyBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LadyBug
+//
+//  Created by Felipe Daguano on 05/11/16.
+//  Copyright © 2016 Hackaton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
