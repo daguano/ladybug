607d47f7d8f909e126c573c7777bf66e10498c07
daguano <daguano@gmail.com>
2016-11-05 18:28:54 -0200

    Login no Facebook

--- LadyBug/LadyBug/ViewController.swift
+++ LadyBug/LadyBug/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var firstTime: Bool = true
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if (FBSDKAccessToken.current() != nil && !firstTime)
+        {
+            
+            //let menu = self.storyboard?.instantiateViewController(withIdentifier: "Menu") as! MenuViewController
+            //self.present(menu, animated: true, completion: nil)
+            
+        }
+        else {
+            firstTime = false
+            
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.frame = CGRect(x: 0.0, y: 0.0, width: loginView.frame.width * 1.5, height: loginView.frame.height * 1.5)
+            loginView.center = self.view.center
+            loginView.center.y += 100
+            loginView.readPermissions = ["public_profile", "email", "user_friends", "read_custom_friendlists"]
+            loginView.delegate = self
+            
+        }
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    // Facebook Delegate Methods
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
+    }
 }
 
