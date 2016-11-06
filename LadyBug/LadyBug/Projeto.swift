d2d8bba091028e5871506dc4c492cc7e4ffd2544
daguano <daguano@gmail.com>
2016-11-06 08:25:28 -0200

    Chamada ao Watson

--- LadyBug/LadyBug/Projeto.swift
+++ LadyBug/LadyBug/Projeto.swift
@@ -21,14 +21,30 @@ class Projeto {
         self.descricao = descricao
         
         // Categorizar o projeto através do Watson
-        let requestURL: NSURL = NSURL(string: "https://dl.dropboxusercontent.com/u/10149729/test.json")!
+        // Natural Language Classifier
+        
+        //curl -G -u "0ade0e1b-8414-4bf4-bd3c-37ba1ade85ce":"AcQlqftXZxI6" "https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers/e82f62x108-nlc-4025/classify" --data-urlencode "text=E as calcinha?"
+        
+        let username = "0ade0e1b-8414-4bf4-bd3c-37ba1ade85ce"
+        let password = "AcQlqftXZxI6"
+        let loginString = String(format: "%@:%@", username, password)
+        let loginData = loginString.data(using: String.Encoding.utf8)!
+        let base64LoginString = loginData.base64EncodedString()
+        
+        let requestURL: NSURL = NSURL(string: "https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers/e82f62x108-nlc-4025/classify?text=calcinha")!
+        
         let urlRequest: NSMutableURLRequest = NSMutableURLRequest(url: requestURL as URL)
+        urlRequest.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+        urlRequest.httpMethod = "GET"
+        
         let session = URLSession.shared
         let task = session.dataTask(with: urlRequest as URLRequest) {
             (data, response, error) -> Void in
             
             let httpResponse = response as! HTTPURLResponse
             let statusCode = httpResponse.statusCode
+            
+            print("request")
             
             if (statusCode == 200) {
                 print("Arquivo baixado")
